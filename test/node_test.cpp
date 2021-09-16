4ad64b35271277d26f1d2a2e8016766ea7efdc83
Shrecki <tericarax@gmail.com>
2021-09-16 12:47:06 +0200

    Start of unit test for Node class

--- test/node_test.cpp
+++ test/node_test.cpp
@@ -34,8 +34,45 @@ TEST_F(NodeTest, DefaultNodeIsLeaf) {
     EXPECT_EQ(testNode.isLeaf(), true);
 }
 
+TEST_F(NodeTest, SomePointerMagic) {
+    Node* testPtr = new Node(50);
+    Node *otherPtr = testPtr;
+    testPtr = nullptr;
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_TRUE(otherPtr != nullptr);
+    EXPECT_EQ(testPtr, nullptr);
+}
+
+TEST_F(NodeTest, nodeSizeAtInit){
+    Node * node = new Node(10);
+    EXPECT_EQ(node->getSize(),0);
+}
+
+TEST_F(NodeTest, setRepresentativeShouldSetCostToZeroWithNoPointsAvailableYet){
+    Node *node = new Node(10);
+    Eigen::VectorXd vector(4);
+    vector << 1, 2, 3,  4;
+    Eigen::VectorXd v2(4);
+    v2 << 1, 2, 7,  4;
+
+    Point p1(&vector);
+    Point p2(&vector);
+
+    EXPECT_EQ(node->getCost(), -1);
+    node->setRepresentative(&p1, Distance::Euclidean);
+    EXPECT_EQ(node->getCost(), 0);
+}
+
+TEST_F(NodeTest, setRepresentativeSetsRepresentativeToCorrectPointer){
+    Node *node = new Node(10);
+    Eigen::VectorXd vector(4);
+    vector << 1, 2, 3,  4;
+
+    Point p1(&vector);
+    node->setRepresentative(&p1, Distance::Euclidean);
+    EXPECT_EQ(node->getRepresentative(), &p1);
+}
+
+TEST_F(NodeTest, addPointCorrectlyUpdatesCost){
+
 }
