859473d5c992b06b5456de581009d6aa8a723b67
Shrecki <tericarax@gmail.com>
2021-09-17 23:09:32 +0200

    Trying transmission of an array of arbitrary size between MATLAB, which calls the mex_test function, and C++. Missing a state machine, to handle TCP communication.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
-#include "src/Node.h"
+#include <zmq.hpp>
+#include <string>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, ZMQ_REP);
+    socket.bind("tcp://*:5555");
+
+    std::string resp("World");
+    zmq::const_buffer message((void*)&resp,5);
+    //socket.send(message, static_cast<zmq::send_flags>(0));
+
+    while(true){
+        zmq::message_t request;
+        socket.recv (&request);
+        // This code allows to read pointer addresses
+        int nElems(request.size());
+        char byteArray[nElems];
+        memcpy(byteArray, request.data(), nElems);
+        //std::string str(byteArray, nElems);
+        //std::cout << str << std::endl;
+        double* array = reinterpret_cast<double*>(byteArray);
+        for(int i=0; i<100;++i){
+            std::cout << array[i] << std::endl;
+
+        }
+
+
+        // Do main algorithmic work here
+
+        zmq::message_t reply(5);
+        memcpy((void *) reply.data(), "World", 5);
+        socket.send(reply);
+    }
+    socket.close();
     return 0;
 }
---
+++ mex_test.cpp
@@ -0,0 +1,48 @@
+#include <zmq.hpp>
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+#include <iostream>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
+
+using namespace matlab::data;
+using matlab::mex::ArgumentList;
+
+#include <sstream> //for std::stringstream
+
+class MexFunction : public matlab::mex::Function {
+public:
+    void operator()(ArgumentList outputs, ArgumentList inputs) {
+        // Now, we will try passing in an array
+        TypedArray<double> doubleArray = std::move(inputs[0]);
+
+        double simpleArray[doubleArray.getNumberOfElements()];
+        for(int i=0; i<doubleArray.getNumberOfElements(); i++){
+            simpleArray[i] = doubleArray[i];
+        }
+
+        // First connect to the port
+        // Then send the request
+        zmq::context_t context(1);
+        zmq::socket_t socket(context, ZMQ_REQ);
+        socket.connect("tcp://localhost:5555");
+
+
+        int n = doubleArray.getNumberOfElements();
+        std::cout << n << std::endl;
+        zmq::message_t request(n*sizeof(double));
+        memcpy((void *) request.data(), (void*)(&simpleArray), n*sizeof(double));
+
+        socket.send(request);
+
+        zmq::message_t reply(5);
+        socket.recv(&reply);
+        std::cout << reply << std::endl;
+        socket.close();
+    }
+
+};
