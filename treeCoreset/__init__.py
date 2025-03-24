cc330cd785a0226bd6e0e423a3755eceb0656a6b
Shrecki <tericarax@gmail.com>
2025-03-24 20:46:06 +0100

    Other attempt at init py fix

--- treeCoreset/__init__.py
+++ treeCoreset/__init__.py
@@ -1,6 +1,6 @@
 # Import the compiled extension module
 try:
-    from .client_coreset import *  # Import all symbols from the extension
+    import client_coreset  # Import all symbols from the extension
     __all__ = ['client_coreset']  # Keep this for backward compatibility
 except ImportError as e:
     import sys
